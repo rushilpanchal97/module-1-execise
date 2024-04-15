fa12b3e9d65fcab278156a8a1daff2102c836e26
Rushil <rushil.panchal@landservices.com.au>
2024-04-15 15:30:41 +0800

    hello

---
+++ calcsquare.ts
@@ -0,0 +1,8 @@
+function calculatesqaure(num: number):number {
+    const squarenum = num * num;
+    return squarenum;
+}
+
+const num = 2;
+const result = calculatesqaure(num)
+console.log(result);
