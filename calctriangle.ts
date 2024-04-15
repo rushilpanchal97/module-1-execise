2e7f52170d960bdd9d571d20e8883490143c2c24
Rushil <rushil.panchal@landservices.com.au>
2024-04-15 15:34:22 +0800

    hi

---
+++ calctriangle.ts
@@ -0,0 +1,9 @@
+const calcTriangleArea = (base: number, height: number): number => {
+    const area  = 2 * base * height;
+    return area
+}
+
+const triangleBase = 10;
+const triangleHeight = 6;
+const trianglearea = calcTriangleArea(triangleBase, triangleHeight);
+console.log(trianglearea);
